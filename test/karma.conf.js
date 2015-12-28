5d8522c49ffa3be80f44d7b6988e8945152e36dd
kodelittle <kode.little@gmail.com>
2015-12-28 19:56:58 +0530

    Trimmed it a bit.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -3,8 +3,8 @@ module.exports = function(config) {
 
 		basePath : '../',
 
-		files : [ 'bower_components/angular/angular.js',
-				'bower_components/angular-mocks/angular-mocks.js',
+		files : [ 'node_modules/angular/angular.js',
+				'node_modules/angular-mocks/angular-mocks.js',
 				'app/js/**/*.js', 'test/unit/**/*.js' ],
 
 		autoWatch : true,
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,14 +1,14 @@
 describe('User list controller.', function() {
 
-	beforeEach(function() {
-		module('myAngularApp'); 
-	});
-
+	
+	beforeEach(module('myAngularApp')) ; 
+	
 	it('should create 2 users', inject(function($controller) {
 		var scope = {};
 		var ctrl = $controller('UserListCtrl', { $scope: scope })
 
 		expect(scope.users.length).toBe(2);
+		// expect(scope.users.length).toBe(3);
 	}));
 
 });
